f80b8ef977dd47c98f8126363d18e6784d55cba4
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-07 17:46:53 +0200

    Fixed html tag in Door component

--- src/components/ui/Door.js
+++ src/components/ui/Door.js
@@ -1,6 +1,6 @@
 function Door() {
   return (
-    <door className="ml-3 mt-6 mb-0 border-black border-4 w-9 h-18 bg-white" />
+    <div className="ml-3 mt-6 mb-0 border-black border-4 w-9 h-18 bg-white" />
   );
 }
 
