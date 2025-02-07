2ccda4b42d6197e2084139c4c877ee8412c95440
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-07 17:55:04 +0200

    Added houses context variable in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,28 @@
+"use client"; // Required for useState in Next.js App Router
+
 import Header from "@/components/layout/Header";
 import Sidebar from "@/components/layout/Sidebar";
 import Building from "@/components/sections/Building";
+import { useHouses } from "@/hooks/useHouses";
 
 export default function Home() {
+  const { houses } = useHouses();
+
   return (
     <div className="flex flex-col max-w-7xl m-auto gap-4">
       <Header />
       <main className="pl-4 flex gap-8">
         <Sidebar />
-        <div className="flex gap-5 grow">
-          <Building />
+        <div className="flex flex-row gap-5 grow justify-center pr-4">
+          {houses.length > 0 ? (
+            <>
+              {houses.map((house, index) => (
+                <Building key={index} house={house} />
+              ))}
+            </>
+          ) : (
+            <p className="text-center">Build your dream city</p>
+          )}
         </div>
       </main>
     </div>
