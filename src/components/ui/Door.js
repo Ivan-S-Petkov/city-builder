0c57c46483fd87498e7f52f13aa2ac73c5b7bd9a
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-07 16:58:47 +0200

    Added Window and Door reuseable components

---
+++ src/components/ui/Door.js
@@ -0,0 +1,7 @@
+function Door() {
+  return (
+    <door className="ml-3 mt-6 mb-0 border-black border-4 w-9 h-18 bg-white" />
+  );
+}
+
+export default Door;
---
+++ src/components/ui/Window.js
@@ -0,0 +1,5 @@
+function Window() {
+  return <div className="mt-4 mb-7 border-black border-2 w-10 h-14 bg-white" />;
+}
+
+export default Window;
