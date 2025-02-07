80ecd957d10b1d13ab7f5e3cddfd3110cb2eed00
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-07 17:55:46 +0200

    Added houses context in Sidebar component

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,25 +1,33 @@
-import Dropdown from "../forms/Dropdown";
-import Input from "../forms/Input";
-import DeleteIcon from "../icons/DeleteIcon";
+import { useHouses } from "@/hooks/useHouses";
 import HomeIcon from "../icons/HomeIcon";
 import HouseForm from "../sections/HouseForm";
 import Button from "../ui/Button";
-import Slider from "../ui/Slider";
 
 function Sidebar() {
+  const { houses, createNewHouse } = useHouses();
+
   return (
     <div className="flex flex-col ">
       <div className="bg-gray-100 border-2 border-gray-200 rounded-t-md p-4">
         <h3 className="text-xl font-bold">Houses List</h3>
       </div>
       <div className="border-l-2 border-r-2 border-gray-200">
-        <HouseForm />
+        {houses.length > 0 ? (
+          <ul>
+            {houses.map((house, index) => (
+              <HouseForm key={index} house={house} />
+            ))}
+          </ul>
+        ) : (
+          <p className="text-center p-[10px] text-sm">Click below to start</p>
+        )}
       </div>
       <div className="bg-gray-100 border-2 border-gray-200 rounded-b-md p-2 flex justify-center">
         <Button
           text="Build a new house"
           icon={<HomeIcon size={20} />}
           className="pl-3 pr-3"
+          clickHandler={createNewHouse}
         />
       </div>
     </div>
