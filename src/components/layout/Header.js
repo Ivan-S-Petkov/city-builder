236d95695617d30cfad79f6ded2780b2924ee404
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-07 12:10:53 +0200

    Added Header layout component

---
+++ src/components/layout/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="bg-gray-100  p-5 border-l-2 border-r-2 border-b-2 border-gray-200 text-red-700 rounded-b-md">
+      <h1 className="text-4xl font-bold">City Builder</h1>
+    </div>
+  );
+}
+
+export default Header;
