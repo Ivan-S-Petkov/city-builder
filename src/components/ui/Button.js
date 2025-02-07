f127ed9c7cf09f9d142c32ae1179172e897261ce
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-07 17:53:54 +0200

    Changed prop name from onClick to clickHandler in Button component

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,8 +1,8 @@
-function Button({ text, icon, onClick, className }) {
+function Button({ text, icon, clickHandler, className }) {
   return (
     <button
       type="button"
-      onClick={onClick}
+      onClick={() => clickHandler()}
       className={`bg-white p-1 border-2 border-gray-200 rounded-md text-sm flex gap-1 ${className}`}
     >
       {icon && icon}
