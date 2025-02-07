6c12a8eb94d8718f7899a76e617bdea99da5ad2f
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-07 16:59:41 +0200

    Added Roof svg icon

---
+++ src/components/icons/RoofIcon.js
@@ -0,0 +1,14 @@
+const RoofIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 60">
+      <path
+        d="M2,59 L50,10 L99,59 Z"
+        fill="none"
+        stroke="black"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+};
+
+export default RoofIcon;
