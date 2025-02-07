6a631b40720eacb74952e4a89594a196e6bc67d2
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-07 15:12:02 +0200

    Added Sidebar to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Header from "@/components/layout/Header";
-import Image from "next/image";
+import Sidebar from "@/components/layout/Sidebar";
 
 export default function Home() {
   return (
     <div className="flex flex-col max-w-7xl m-auto gap-4">
       <Header />
+      <main className="pl-4 flex gap-8">
+        <Sidebar />
+      </main>
     </div>
   );
 }
