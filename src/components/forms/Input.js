a9b79002d6d6078bd2c37e457c40c48c5ece592d
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-08 14:52:12 +0200

    Added value, className, changeHandler, blurHandler and ref as props in Input component

--- src/components/forms/Input.js
+++ src/components/forms/Input.js
@@ -1,15 +1,33 @@
-function Input({ label, type, min = 1, max = 999 }) {
+import { forwardRef } from "react";
+
+const Input = forwardRef(function Input(
+  {
+    label,
+    type,
+    min = 1,
+    max = 999,
+    value,
+    className,
+    changeHandler = () => {},
+    blurHandler,
+  },
+  ref
+) {
   return (
     <div>
-      <label className="mr-1">{label}:</label>
+      {label && <label className="mr-1">{label}:</label>}
       <input
+        ref={ref}
+        value={value}
         type={type}
         min={min}
         max={max}
-        className="w-10 border-gray-200 border-2"
+        className={`w-10 border-gray-200 border-2 ${className} ml-[3px]`}
+        onChange={(e) => changeHandler(e.target.value)}
+        onBlur={blurHandler}
       />
     </div>
   );
-}
+});
 
 export default Input;
