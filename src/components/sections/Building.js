0e7a82f0608c4dbe40a3e38068d5da0dd4c199b4
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-08 14:56:02 +0200

    Added house as props in Building component. Added enter and exit animation for floors and building

--- src/components/sections/Building.js
+++ src/components/sections/Building.js
@@ -1,14 +1,38 @@
 import RoofIcon from "../icons/RoofIcon";
 import Floor from "../ui/Floor";
 import GroundFloor from "../ui/GroundFloor";
+import { AnimatePresence, motion } from "framer-motion";
 
-function Building() {
+function Building({ house }) {
   return (
-    <div className="flex flex-col min-w-[40px] w-[25%]">
+    <motion.div
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.8, y: 50 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      className="flex flex-col min-w-[40px] w-[25%]"
+    >
       <RoofIcon />
-      <Floor color="#fa2354" />
-      <GroundFloor color="#38974d" />
-    </div>
+      <div className="flex flex-col-reverse">
+        <AnimatePresence>
+          {house.floors.map((floor, i) =>
+            i === 0 ? (
+              <GroundFloor key={i} color={floor} />
+            ) : (
+              <motion.div
+                key={i}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 20 }}
+                transition={{ duration: 0.5, delay: i * 0.1 }}
+              >
+                <Floor key={i} color={floor} />
+              </motion.div>
+            )
+          )}
+        </AnimatePresence>
+      </div>
+    </motion.div>
   );
 }
 
