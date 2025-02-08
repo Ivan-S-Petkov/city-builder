48be2aa2eddfd86a549a2b82cd2c6b06ff8cd58a
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-08 14:58:44 +0200

    Added value and handleChange props to Slider component.

--- src/components/ui/Slider.js
+++ src/components/ui/Slider.js
@@ -1,4 +1,4 @@
-function Slider({ floors }) {
+function Slider({ floors, value, handleChange }) {
   const steps = [];
   for (let i = 0; i < floors; i++) {
     steps.push(<li key={i} className="flex justify-center relative" />);
@@ -6,7 +6,15 @@ function Slider({ floors }) {
 
   return (
     <div className="flex flex-col space-y-2 p-2 w-40">
-      <input type="range" className="w-full" min="1" max={floors} step="1" />
+      <input
+        type="range"
+        className="w-full"
+        min="1"
+        max="10"
+        step="1"
+        defaultValue={value}
+        onChange={(e) => handleChange(e.target.value)}
+      />
       <ul className="flex justify-between w-full px-[10px]">{steps}</ul>
     </div>
   );
