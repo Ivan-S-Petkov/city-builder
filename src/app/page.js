d3e3675fe68dbf2af00b72f4f498bc0da266ea14
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-08 16:46:15 +0200

    Added WeatherWidged to home page

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 import Header from "@/components/layout/Header";
 import Sidebar from "@/components/layout/Sidebar";
 import Building from "@/components/sections/Building";
+import WeatherWidget from "@/components/ui/WeatherWidget";
 import { useHouses } from "@/hooks/useHouses";
 import { AnimatePresence } from "framer-motion";
 
@@ -14,16 +15,19 @@ export default function Home() {
       <Header />
       <main className="pl-4 flex gap-8">
         <Sidebar />
-        <div className="flex flex-row self-start gap-5 grow justify-center pr-4 items-end">
-          {houses.length > 0 ? (
-            <AnimatePresence>
-              {houses.map((house) => (
-                <Building key={house.id} house={house} />
-              ))}
-            </AnimatePresence>
-          ) : (
-            <p className="text-center self-start">Build your dream city</p>
-          )}
+        <div className="relative flex flex-col items-start gap-[10px] grow  pr-4 ">
+          <WeatherWidget />
+          <div className="flex flex-row gap-5 items-end justify-center w-[100%]">
+            {houses.length > 0 ? (
+              <AnimatePresence>
+                {houses.map((house) => (
+                  <Building key={house.id} house={house} />
+                ))}
+              </AnimatePresence>
+            ) : (
+              <p className="text-center self-start">Build your dream city</p>
+            )}
+          </div>
         </div>
       </main>
     </div>
