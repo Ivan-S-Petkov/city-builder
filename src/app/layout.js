d38de8ee751b247a49026f10d392887502a1867a
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-08 16:46:58 +0200

    Added WeatherProvider to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import HousesProvider from "../context/HousesContext";
+import WeatherProvider from "@/context/WeatherContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <HousesProvider>{children}</HousesProvider>
+        <HousesProvider>
+          <WeatherProvider>{children}</WeatherProvider>
+        </HousesProvider>
       </body>
     </html>
   );
