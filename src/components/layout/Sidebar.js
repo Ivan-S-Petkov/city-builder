5aa698b619613966cfa8520e58899a4982ce2669
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-08 14:53:19 +0200

    Implemented limit to build up to 7 houses in Sidebar component and updated styles

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -7,28 +7,34 @@ function Sidebar() {
   const { houses, createNewHouse } = useHouses();
 
   return (
-    <div className="flex flex-col ">
+    <div className="w-[340px] flex flex-col shrink-0">
       <div className="bg-gray-100 border-2 border-gray-200 rounded-t-md p-4">
         <h3 className="text-xl font-bold">Houses List</h3>
       </div>
       <div className="border-l-2 border-r-2 border-gray-200">
         {houses.length > 0 ? (
-          <ul>
+          <div>
             {houses.map((house, index) => (
-              <HouseForm key={index} house={house} />
+              <HouseForm key={house.id} index={index} house={house} />
             ))}
-          </ul>
+          </div>
         ) : (
           <p className="text-center p-[10px] text-sm">Click below to start</p>
         )}
       </div>
       <div className="bg-gray-100 border-2 border-gray-200 rounded-b-md p-2 flex justify-center">
-        <Button
-          text="Build a new house"
-          icon={<HomeIcon size={20} />}
-          className="pl-3 pr-3"
-          clickHandler={createNewHouse}
-        />
+        {houses.length < 7 ? (
+          <Button
+            text="Build a new house"
+            icon={<HomeIcon size={20} />}
+            className="pl-3 pr-3"
+            clickHandler={createNewHouse}
+          />
+        ) : (
+          <p className="text-center">
+            You reached maximum houses in this neighbourhood
+          </p>
+        )}
       </div>
     </div>
   );
