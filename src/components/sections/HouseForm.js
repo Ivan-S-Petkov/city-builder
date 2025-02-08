7121388e21f44279bfc9a0a11b015f4e53913f97
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-08 15:04:13 +0200

    Added index and house as props, updateFloors, updateColor and updateName functions in HouseForm component.

--- src/components/sections/HouseForm.js
+++ src/components/sections/HouseForm.js
@@ -1,26 +1,85 @@
-import Dropdown from "../forms/Dropdown";
-import Input from "../forms/Input";
+import { useHouses } from "@/hooks/useHouses";
 import DeleteIcon from "../icons/DeleteIcon";
 import Button from "../ui/Button";
 import Slider from "../ui/Slider";
+import Input from "../forms/Input";
+import HouseName from "../ui/HouseName";
+import CopyIcon from "../icons/CopyIcon";
 
-function HouseForm() {
+function HouseForm({ index, house }) {
+  const { houses, deleteHouse, updateHouse, copyHouse } = useHouses();
+
+  const updateFloors = (floors) => {
+    // Check if we should add aditional values in floors array or slice them
+    if (floors > house.floors.length) {
+      for (let i = house.floors.length; i < floors; i++) {
+        house.floors.push(house.color);
+      }
+    } else {
+      let floorsArr = house.floors.slice(0, floors);
+      house.floors = floorsArr;
+      house.color = house.floors[house.floors.length - 1];
+    }
+    updateHouse(index, house);
+  };
+
+  const updateColor = (color) => {
+    house.color = color;
+    house.floors[house.floors.length - 1] = color;
+    updateHouse(index, house);
+  };
+
+  const updateName = (name) => {
+    house.name = name.trim();
+    updateHouse(index, house);
+  };
+
   return (
     <div className="flex p-3 gap-6 items-start">
       <div className="flex flex-col justify-between">
-        <h4 className="font-bold">House 1</h4>
+        <HouseName
+          houseName={house.name}
+          index={index}
+          updateName={updateName}
+        />
         <div className="flex text-sm font-bold">
           <div className="flex flex-col">
-            <Input label="Floors" type="number" max={5} />
-            <Slider floors={10} />
+            <div className="flex items-center">
+              <label className="mr-1">Floors:</label>
+              <div className="w-10 border-gray-200 border-2">
+                {house.floors.length}
+              </div>
+            </div>
+            <Slider
+              floors={10}
+              value={house.floors.length}
+              handleChange={updateFloors}
+            />
           </div>
-          <Dropdown
+          <Input
+            type="color"
             label="Color"
-            options={["Orange", "Alizarin", "Belize", "Emarald"]}
+            value={house.color}
+            changeHandler={updateColor}
           />
         </div>
       </div>
-      <Button icon={<DeleteIcon size={20} />} />
+      <div>
+        {houses.length < 7 && (
+          <Button
+            icon={<CopyIcon size={20} />}
+            clickHandler={() => {
+              copyHouse(house);
+            }}
+          />
+        )}
+        <Button
+          icon={<DeleteIcon size={20} />}
+          clickHandler={() => {
+            deleteHouse(index);
+          }}
+        />
+      </div>
     </div>
   );
 }
