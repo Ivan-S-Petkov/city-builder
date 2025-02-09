b20adee392cfd6b5136b367e4b138f12e606c957
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-10 00:39:58 +0200

    Split Houses context to houses state and actions

--- src/context/HousesContext.js
+++ src/context/HousesContext.js
@@ -1,10 +1,17 @@
 "use client";
 
-import { createContext, useState, useEffect } from "react";
+import {
+  createContext,
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+} from "react";
 import { v4 as uuidv4 } from "uuid";
 
-// Create the context
-export const HousesContext = createContext();
+// Create the contexts
+export const HousesStateContext = createContext();
+export const HousesActionsContext = createContext();
 
 // Provider component
 export default function HousesProvider({ children }) {
@@ -23,41 +30,44 @@ export default function HousesProvider({ children }) {
     localStorage.setItem("houses", JSON.stringify(houses));
   }, [houses]);
 
-  // Create new house
-  const createNewHouse = () => {
-    const house = {
-      id: uuidv4(),
-      name: "House",
-      floors: ["#ffffff"],
-      color: "#ffffff",
+  const actions = useMemo(() => {
+    return {
+      createNewHouse: () => {
+        setHouses((prev) => [
+          ...prev,
+          {
+            id: uuidv4(),
+            name: "House",
+            floors: ["#ffffff"],
+            color: "#ffffff",
+          },
+        ]);
+      },
+
+      updateHouse: (index, newHouse) => {
+        setHouses((prevHouses) =>
+          prevHouses.map((house, i) => (i === index ? newHouse : house))
+        );
+      },
+
+      deleteHouse: (index) => {
+        setHouses((prev) => prev.filter((_, i) => i !== index));
+      },
+
+      copyHouse: (house) => {
+        setHouses((prev) => [
+          ...prev,
+          { ...house, floors: [...house.floors], id: uuidv4() },
+        ]);
+      },
     };
-    setHouses((prev) => [...prev, house]);
-  };
-
-  // Update house my index
-  const updateHouse = (index, house) => {
-    const houseArr = [...houses];
-    houseArr[index] = house;
-    setHouses(houseArr);
-  };
-
-  // Delete house by index
-  const deleteHouse = (index) => {
-    setHouses(houses.filter((house, i) => i != index));
-  };
-
-  // Duplicate house
-  const copyHouse = (house) => {
-    // Create new array for floors and new unique id
-    let newHouse = { ...house, floors: [...house.floors], id: uuidv4() };
-    setHouses((prev) => [...prev, newHouse]);
-  };
+  }, []);
 
   return (
-    <HousesContext.Provider
-      value={{ houses, createNewHouse, updateHouse, deleteHouse, copyHouse }}
-    >
-      {children}
-    </HousesContext.Provider>
+    <HousesStateContext.Provider value={houses}>
+      <HousesActionsContext.Provider value={actions}>
+        {children}
+      </HousesActionsContext.Provider>
+    </HousesStateContext.Provider>
   );
 }
