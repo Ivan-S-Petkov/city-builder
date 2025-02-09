a7524860f7f16e7d4d3e070d4dca8781f9e580ee
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-02-10 00:40:41 +0200

    Split useHouses hook to useHousesState and useHousesActions

--- src/hooks/useHouses.js
+++ src/hooks/useHouses.js
@@ -1,12 +1,23 @@
-import { HousesContext } from "@/context/HousesContext";
+import {
+  HousesActionsContext,
+  HousesStateContext,
+} from "@/context/HousesContext";
 import { useContext } from "react";
 
-export function useHouses() {
-  const context = useContext(HousesContext);
-
+// Custom hook to access houses state
+export function useHousesState() {
+  const context = useContext(HousesStateContext);
   if (!context) {
-    throw new Error("useHouses must be used within a HousesProvider");
+    throw new Error("useHousesState must be used within a HousesProvider");
   }
+  return context;
+}
 
+// Custom hook to access houses actions (functions)
+export function useHousesActions() {
+  const context = useContext(HousesActionsContext);
+  if (!context) {
+    throw new Error("useHousesActions must be used within a HousesProvider");
+  }
   return context;
 }
